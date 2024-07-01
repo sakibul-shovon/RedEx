50871c1ac3f568d0d10307e0414e99ad67f32400
DESKTOP-0POIF7H\mdift <md.iftekharzawad@gmail.com>
2024-07-02 04:11:04 +0600

    working login page and registration page using firebase

--- lib/Pages/registerPage.dart
+++ lib/Pages/registerPage.dart
@@ -2,10 +2,12 @@
 import 'package:flutter/material.dart';
 import 'package:redex/Components/MyButton.dart';
 import 'package:redex/Components/MyTextField.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'home_page.dart';
 
 class RegisterPage extends StatefulWidget {
   final void Function()? onTap;
-  RegisterPage({super.key, required this.onTap});
+  const RegisterPage({super.key, required this.onTap});
 
   @override
   State<RegisterPage> createState() => _RegisterPageState();
@@ -15,32 +17,46 @@ class _RegisterPageState extends State<RegisterPage> {
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
   final TextEditingController confirmPasswordController =
-      TextEditingController();
+  TextEditingController();
+  final FirebaseAuth _auth = FirebaseAuth.instance;
 
-  void registerUser() {
-    // Implement registration logic here
-    String email = emailController.text;
-    String password = passwordController.text;
-    String confirmPassword = confirmPasswordController.text;
+  void registerUser() async {
+    String email = emailController.text.trim();
+    String password = passwordController.text.trim();
+    String confirmPassword = confirmPasswordController.text.trim();
 
     if (password != confirmPassword) {
-      // Show error if passwords do not match
-      showDialog(
-        context: context,
-        builder: (context) {
-          return AlertDialog(
-            content: Text("Passwords do not match!"),
-          );
-        },
-      );
+      showError("Passwords do not match!");
       return;
     }
 
-    // Continue with registration logic
-    print("Email: $email");
-    print("Password: $password");
+    try {
+      UserCredential userCredential = await _auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      if (userCredential.user != null) {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const HomePage(),
+          ),
+        );
+      }
+    } on FirebaseAuthException catch (e) {
+      showError(e.message ?? 'An error occurred');
+    }
+  }
 
-    // Call your registration API or perform other actions
+  void showError(String errorMessage) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          content: Text(errorMessage),
+        );
+      },
+    );
   }
 
   @override
@@ -52,62 +68,43 @@ class _RegisterPageState extends State<RegisterPage> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              // Logo
               Icon(
                 Icons.lock_open_rounded,
                 size: 100,
                 color: Theme.of(context).colorScheme.primary,
               ),
-
               SizedBox(height: 25),
-
-              // App slogan
               Text(
-                'Food App',
+                'RedEx',
                 style: TextStyle(
                   fontSize: 16,
                   color: Theme.of(context).colorScheme.primary,
                 ),
               ),
-
               SizedBox(height: 25),
-
-              // Email text field
               MyTextField(
                 controller: emailController,
                 hintText: 'Email',
                 obscureText: false,
               ),
-
               SizedBox(height: 25),
-
-              // Password text field
               MyTextField(
                 controller: passwordController,
                 hintText: 'Password',
                 obscureText: true,
               ),
-
               SizedBox(height: 25),
-
-              // Confirm Password text field
               MyTextField(
                 controller: confirmPasswordController,
                 hintText: 'Confirm Password',
                 obscureText: true,
               ),
-
               SizedBox(height: 25),
-
-              // Register button
               MyButton(
                 text: 'Register',
                 onTap: registerUser,
               ),
-
               SizedBox(height: 25),
-
-              // Login section
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
@@ -136,3 +133,4 @@ class _RegisterPageState extends State<RegisterPage> {
     );
   }
 }
+
