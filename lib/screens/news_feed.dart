83f813e0384dec4b2335bb4b409bf651017a27e1
plasma-gith <md.iftekharzawad@gmail.com>
2024-09-24 00:42:18 +0600

    newsfeed added

---
+++ lib/screens/news_feed.dart
@@ -0,0 +1,39 @@
+import 'dart:io';
+
+import 'package:chting_app/api/apis.dart';
+import 'package:chting_app/helper/dialogs.dart';
+import 'package:chting_app/screens/auth/login_screen.dart';
+import 'package:chting_app/screens/home_screen.dart';
+import 'package:chting_app/screens/upload_file_screen.dart';
+import 'package:chting_app/services/database_controller.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:image_picker/image_picker.dart';
+
+class NewsFeedPage extends StatefulWidget {
+  const NewsFeedPage({Key? key}) : super(key: key);
+
+  @override
+  _NewsFeedPageState createState() => _NewsFeedPageState();
+}
+
+class _NewsFeedPageState extends State<NewsFeedPage> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  final TextEditingController _titleController = TextEditingController();
+  final TextEditingController _descriptionController = TextEditingController();
+  final ImagePicker _imagePicker = ImagePicker();
+  File? _imageFile;
+  final DatabaseController _databaseController = DatabaseController();
+
+
+
+
+
+
+
+
+
+
+
